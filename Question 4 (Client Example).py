3a6fd5b9bbe89aad9670163d97754fddc2315659
MeorHafizuddeen <129260557+MeorHafizuddeen@users.noreply.github.com>
2023-07-20 22:17:26 +0800

    Create Question 4 (Client Example).py

---
+++ Question 4 (Client Example).py
@@ -0,0 +1,29 @@
+import socket
+
+def get_quote():
+    server_address = '127.0.0.1'  # Change this to the server IP address if needed
+    server_port = 8888
+    buffer_size = 1024
+
+    # Create a socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to the server
+        client_socket.connect((server_address, server_port))
+
+        # Receive the quote from the server
+        quote = client_socket.recv(buffer_size).decode()
+
+        print(f"Random Quote of the Day:\n {quote}")
+
+    except ConnectionRefusedError:
+        print("Connection refused. Make sure the server is running and accessible.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    # Close the client socket
+    client_socket.close()
+
+if __name__ == "__main__":
+    get_quote()
